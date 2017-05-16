6b68f7834236322421f292cd8934d9963b039649
kabutz <heinz@javaspecialists.eu>
2017-05-16 11:50:59 +0300

    demo 3 with timing

--- forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
+++ forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
@@ -20,17 +20,23 @@ public class Fibonacci {
 
         RecursiveTask<BigInteger> f0_task = new RecursiveTask<BigInteger>() {
             protected BigInteger compute() {
-                return f(half-1);
+                return f(half - 1);
             }
         };
         f0_task.fork();
         BigInteger f1 = f(half);
         BigInteger f0 = f0_task.join();
 
-        if (n % 2 == 1) {
-            return f0.multiply(f0).add(f1.multiply(f1));
-        } else {
-            return f0.shiftLeft(1).add(f1).multiply(f1);
+        long time = n > 10_000 ? System.currentTimeMillis() : 0;
+        try {
+            if (n % 2 == 1) {
+                return f0.multiply(f0).add(f1.multiply(f1));
+            } else {
+                return f0.shiftLeft(1).add(f1).multiply(f1);
+            }
+        } finally {
+            time = n > 10_000 ? System.currentTimeMillis() - time : 0;
+            if (time > 50) System.out.printf("f(%d) took %d%n", n, time);
         }
     }
 }
